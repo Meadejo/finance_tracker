c1a045c7696e4e81a0ff4a8d65623cb55fc43edd
Meadejo <45802964+Meadejo@users.noreply.github.com>
2025-05-04 17:48:31 -0400

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ class Config:
     """
     TODOC
     """
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'fuck-me-in-the-ass-with-a-posthole-digger'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-a-very-secret-key-and-i-have-every-reasonable-confidence-that-nobody-could-ever-guess-it'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'database/test.db')
